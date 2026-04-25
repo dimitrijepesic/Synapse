b8a9f6c7ec2f0f7f9e4fbf4e0438c67ac3e194c5
Nenad Skokovic <nenad.skokovic@gmail.com>
2026-04-25 18:08:25 +0200

    Add 3 nice tests

--- backend/tests/parser_tests/test.py
+++ backend/tests/parser_tests/test.py
@@ -1,12 +1,31 @@
+# import sys
+# sys.path.insert(0, "/home/nenad/work/hackupc/hackupc/backend")
+
+# import json
+# from parser import parse_repo
+
+# result = parse_repo("parser_tests/katana-swift", repo_name="BendingSpoons/katana-swift")
+
+# with open("parser_tests/output.json", "w") as f:
+#     json.dump(result, f, indent=2)
+
+# print("Done! Check test/output.json")
+
+# test.py
 import sys
 sys.path.insert(0, "/home/nenad/work/hackupc/hackupc/backend")
 
 import json
 from parser import parse_repo
 
-result = parse_repo("parser_tests/katana-swift", repo_name="BendingSpoons/katana-swift")
+test_folders = ["test1", "test2", "test3"]
 
-with open("parser_tests/output.json", "w") as f:
-    json.dump(result, f, indent=2)
-
-print("Done! Check test/output.json")
+for folder in test_folders:
+    print(f"Parsing {folder}...")
+    result = parse_repo(folder, repo_name=folder)
+    
+    output_file = f"{folder}_output.json"
+    with open(output_file, "w") as f:
+        json.dump(result, f, indent=2)
+    
+    print(f"Done! Check {output_file}")
